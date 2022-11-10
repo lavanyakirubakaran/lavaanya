c0857805bf2e419025da54ee8c85754aab53374a
lavanyarajendran <116621788+lavanyakirubakaran@users.noreply.github.com>
2022-11-10 13:20:45 +0530

    Add files via upload

---
+++ String_split.java
@@ -0,0 +1,12 @@
+package com.spillted;
+
+public class String_split {
+	public static void main(String args[]) {
+		String s="lavanya mobile center";
+		String[] L=s.split(" ");
+		for(int i=0; i<L.length; i++)
+		System.out.println(L[i]+" ");
+		
+		}
+
+}

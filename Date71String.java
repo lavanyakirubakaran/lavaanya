552f2e0c1d0bb0db7710727c30b97dc7fe3d4603
lavanyarajendran <116621788+lavanyakirubakaran@users.noreply.github.com>
2022-11-10 06:27:30 +0530

    Add files via upload

---
+++ Date71String.java
@@ -0,0 +1,15 @@
+package com.stringbuffer;
+
+public class Date71String {
+	public static void main(String lavan[]) {
+		String s=new String("lavanya");
+		System.out.println(s);
+		System.out.println(s.concat(" software engineer"));
+		System.out.println();
+		StringBuffer sd=new StringBuffer("lavan");
+		System.out.println(sd);
+		System.out.println(sd.append(" mobile"));
+	
+	}
+
+}

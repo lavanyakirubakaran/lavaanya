cb9f8918040e8e63eb2c9a2c924c7222f835a665
lavanyarajendran <116621788+lavanyakirubakaran@users.noreply.github.com>
2022-11-10 18:37:04 +0530

    Add files via upload

---
+++ Builder.java
@@ -0,0 +1,11 @@
+package com.spillted;
+
+public class Builder {
+	public static void main(String[] lava){
+		StringBuilder sb=new StringBuilder("Lavanya");
+		System.out.println(sb);
+		System.out.println(sb.append(" Software engineerS"));
+		System.out.println(sb.reverse());
+		
+	}
+}

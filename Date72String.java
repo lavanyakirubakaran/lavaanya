ea744de5bb118dedcfb08619e35283940268bdd9
lavanyarajendran <116621788+lavanyakirubakaran@users.noreply.github.com>
2022-11-10 06:37:53 +0530

    Add files via upload

---
+++ Date72String.java
@@ -0,0 +1,12 @@
+package com.indexof;
+
+public class Date72String {
+	public static void main(String lava[]) {
+		String a="lavanya";
+		System.out.println(a);
+		System.out.println(a.indexOf("n"));
+		System.out.println(a.lastIndexOf("y"));
+		System.out.println(a.indexOf("m"));
+	}
+
+}

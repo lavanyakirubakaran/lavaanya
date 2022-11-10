0090e48775c1c855a4311e153f52a06b7cd4987d
lavanyarajendran <116621788+lavanyakirubakaran@users.noreply.github.com>
2022-11-10 13:25:42 +0530

    Add files via upload

---
+++ String_reverse.java
@@ -0,0 +1,12 @@
+package com.spillted;
+
+public class String_reverse {
+	public static void main(String args[]) {
+		String s="My name is lavanya";
+		String[] R=s.split(" ");
+		for(int i=R.length-1; i>=0; i--)
+		System.out.print(R[i]+" ");
+		
+		}
+
+}

263c2f9674bd4bc680ce8042762acbc2980f387b
lavanyarajendran <116621788+lavanyakirubakaran@users.noreply.github.com>
2022-11-29 19:31:25 +0530

    Add files via upload

---
+++ Arms.java
@@ -0,0 +1,18 @@
+package com.java1;
+import java.util.Scanner;
+
+public class Arms {
+	public static void main(String lava[]) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the table : ");
+		int t=in.nextInt();
+		System.out.println("Enter the limit : ");
+		int n=in.nextInt();
+		for(int i=1;i<=n;i++) {
+			
+			System.out.println(t+"x"+i+"="+(t*i));
+				
+		
+	}}
+
+}
